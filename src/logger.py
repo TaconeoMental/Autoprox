c1d865fd8b962045aaa6f19e9da03c76afd7b4e4
Mateo Contenla <25857822+TaconeoMental@users.noreply.github.com>
2022-09-23 02:11:31 -0300

    Inicios del proxy

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,47 @@
+(
+    LEVEL_OFF,
+    LEVEL_GOOD,
+    LEVEL_ERROR,
+    LEVEL_WARN,
+    LEVEL_INFO,
+    LEVEL_DEBUG
+) = range(6)
+
+class Colour:
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    BOLD = '\033[1m'
+    GREEN = '\033[92m'
+    ENDC = '\033[0m'
+
 class Logger:
     def __init__(self):
-        pass
+        self.set_level(LEVEL_DEBUG)
+
     def quiet(self):
-        pass
+        self.set_level(LEVEL_OFF)
+
     def set_level(self, verbose_level):
-        pass
-    pass
+        if LEVEL_OFF <= verbose_level <= LEVEL_DEBUG:
+            self.level = verbose_level
+
+    def _print(self, level, level_name, fmt, *args):
+        if level <= self.level:
+            print(f"{level_name} {fmt}{Colour.ENDC}".format(*args))
+
+    def DEBUG(self, fmt, *args):
+        self._print(LEVEL_DEBUG, "[D]", fmt, *args)
+
+    def INFO(self, fmt, *args):
+        self._print(LEVEL_INFO, f"{Colour.BOLD}[*]", fmt, *args)
+
+    def WARN(self, fmt, *args):
+        self._print(LEVEL_WARN, f"{Colour.YELLOW}[!]", fmt, *args)
+
+    def ERROR(self, fmt, *args):
+        self._print(LEVEL_ERROR, f"{Colour.RED}[-]", fmt, *args)
+
+    def GOOD(self, fmt, *args):
+        self._print(LEVEL_GOOD, f"{Colour.GREEN}[+]", fmt, *args)
 
 LOGGER = Logger()
--- src/proxy.py
+++ src/proxy.py
@@ -1,3 +1,75 @@
+import os
+import os.path as op
+import subprocess
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from functools import partial
+
+from .logger import LOGGER
+
+CERTS_DIR = "certs"
+CACERT_PATH = op.join(CERTS_DIR, "cacert.crt")
+CAKEY_PATH = op.join(CERTS_DIR, "cakey.key")
+CERTKEY_PATH = op.join(CERTS_DIR, "cert.key")
+
+def check_certificates(directory):
+    return op.exists(CACERT_PATH) and op.exists(CAKEY_PATH) and op.exists(CERTKEY_PATH)
+
+def generate_certificates(directory):
+    ca_name = "Autoprox CA"
+    if not op.exists(directory):
+        os.mkdir(directory)
+    subprocess.run(['openssl', 'genrsa', '-out', CERTKEY_PATH, '2048'],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+    subprocess.run(['openssl', 'genrsa', '-out', CAKEY_PATH, '2048'],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+    subprocess.run(['openssl', 'req', '-new', '-x509', '-days', '3650', '-key', CAKEY_PATH, '-out', CACERT_PATH, '-subj', f"/CN={ca_name}"],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
+
+
+class ProxyRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, ast, *args, **kwargs):
+        self.filter_ast = ast
+        super().__init__(*args, **kwargs)
+
+    def do_CONNECT(self):
+        LOGGER.DEBUG("hola")
+
+    def do_GET(self):
+        if self.path == "http://autoprox/":
+            self.send_file("certs/cacert.crt")
+            return
+
+
+    def send_file(self, path):
+        with open(path, "rb") as f:
+            data = f.read()
+        self.send_response(200)
+        self.send_header("Content-Length", len(data))
+        self.send_header("Content-Disposition", f"attachment; filename={op.basename(path)}")
+        self.end_headers()
+        self.wfile.write(data)
+
 class Proxy:
-    def __init__(self):
+    def __init__(self, bind, port):
+        if not check_certificates(CERTS_DIR):
+            LOGGER.INFO("Creating certificates")
+            generate_certificates(CERTS_DIR)
+
+        self.bind = bind
+        self.port = port
+
+    def set_config_file(self, src):
         pass
+
+    def run(self):
+        address = (self.bind, self.port)
+        try:
+            handler = partial(ProxyRequestHandler, "test")
+
+            LOGGER.INFO("Serving proxy on {}:{}", *address)
+            httpd = HTTPServer(address, handler)
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            LOGGER.ERROR("Shutting down proxy")
+            httpd.shutdown()
+            httpd.server_close()
